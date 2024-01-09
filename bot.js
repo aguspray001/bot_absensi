212f75c390bf69d94806de89f004d31a860f9770
aguspray001 <aguspray001@gmail.com>
2024-01-09 13:49:31 +0700

    first commit

---
+++ bot.js
@@ -0,0 +1,62 @@
+import puppeteer from "puppeteer";
+
+export const botAbsensi = async (url, email, password, presensi=false) => {
+  //open web
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+  const page = await browser.newPage();
+  await page.evaluateOnNewDocument(() => {
+    navigator.geolocation.getCurrentPosition = (cb) => {
+      setTimeout(() => {
+        cb({
+          coords: {
+            accuracy: 21,
+            altitude: null,
+            altitudeAccuracy: null,
+            heading: null,
+            latitude: -7.3229351,
+            longitude: 112.7362878,
+            speed: null,
+          },
+        });
+      }, 1000);
+    };
+  });
+  await page.goto(url);
+  //fill login form
+  await page.type("#email", email);
+  await page.type("#password", password);
+  await page.waitForTimeout(500);
+  //click login
+  await Promise.all([
+    page.waitForNavigation(), // The promise resolves after navigation has finished
+    page.click("#form-login > div.row > div.col-sm-4.text-end > button"), // Clicking the link will indirectly cause a navigation
+  ]);
+  //open dropdown
+  const burgerMenuSelector = "#app > div > nav > div > form > ul > li > a";
+  await page.waitForSelector(burgerMenuSelector);
+  await page.click(burgerMenuSelector);
+  await page.click(burgerMenuSelector);
+
+  //click pengguna
+  const penggunaMenuSelector =
+    "#sidebar-wrapper > ul > li.nav-item.dropdown > a";
+
+  await Promise.all([
+    page.waitForSelector(penggunaMenuSelector),
+    page.click(penggunaMenuSelector),
+    page.waitForTimeout(500),
+  ]);
+  //click presensi menu
+  const presensiMenuSelector =
+    "#sidebar-wrapper > ul > li.nav-item.dropdown.active > ul > li:nth-child(2) > a";
+  await page.waitForSelector(presensiMenuSelector),
+    await page.click(presensiMenuSelector);
+
+  if (presensi === true) {
+    //click presensi button
+    //page.click("#app > div > div.main-content > section > div.section-body > div.row.mt-sm-4 > div > div > div > form > button")
+  }
+};
---
+++ index.js
@@ -0,0 +1,33 @@
+import { CronJob } from "cron";
+import { botAbsensi } from "./bot.js";
+import dotenv from 'dotenv'
+dotenv.config()
+
+const d = new Date();
+let current_minutes = d.getMinutes();
+
+const menit_datang = current_minutes;
+const jam_datang = '7';
+const hari_datang = "1-5";
+
+const menit_pulang = current_minutes;
+const jam_pulang = '16';
+const hari_pulang = "1-5";
+
+const jobAbsenDatang = CronJob.from({
+  cronTime: `0 ${menit_datang} ${jam_datang} * * ${hari_datang}`,
+  onTick: ()=>botAbsensi(process.env.URL, process.env.EMAIL_USER, process.env.PASSWORD_USER, false),
+  start: true,
+  timeZone: "Asia/Jakarta",
+});
+
+jobAbsenDatang.start();
+
+const jobAbsenPulang = CronJob.from({
+  cronTime: `0 ${menit_pulang} ${jam_pulang} * * ${hari_pulang}`,
+  onTick: ()=>botAbsensi(process.env.URL, process.env.EMAIL_USER, process.env.PASSWORD_USER, false),
+  start: true,
+  timeZone: "Asia/Jakarta",
+});
+
+jobAbsenPulang.start();
